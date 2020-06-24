db9e8d6144a8564322bda0145f42b1475cdeabe2
Sheikh Saad Sristy <sristy.cse@gmail.com>
2020-06-24 13:07:41 +0600

    first
    initial commit

---
+++ Assets/Scripts/PlayerSettings.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PlayerSettings {
+
+   private static int cubeSize;
+   private static float scrambleRotationTime;
+   private static int scrambleTimes;
+   private static bool settingsOn;
+   private static bool gameWon;
+   private static bool timerOn;
+   private static bool cameraDisable;
+   private static bool faceRotation;
+   private static bool cubeRotation;
+   private static bool gyroOn;
+   private static bool scrambling;
+
+
+    public static int ScrambleTimes
+    {
+        get
+        {
+            return scrambleTimes;
+        }
+        set
+        {
+            scrambleTimes = value;
+        }
+    }
+
+    public static float ScrambleRotationTime
+    {
+        get
+        {
+            return scrambleRotationTime;
+        }
+        set
+        {
+            scrambleRotationTime = value;
+        }
+    }
+
+   public static int CubeSize {
+      get {
+         return cubeSize;
+      }
+      set {
+         cubeSize = value;
+      }
+   }
+   public static bool SettingsOn {
+      get {
+         return settingsOn;
+      }
+      set {
+         settingsOn = value;
+      }
+   }
+   public static bool GameWon {
+      get {
+         return gameWon;
+      }
+      set {
+         gameWon = value;
+      }
+   }
+   public static bool TimerOn {
+      get {
+         return timerOn;
+      }
+      set {
+         timerOn = value;
+      }
+   }
+   public static bool CameraDisable {
+      get {
+         return cameraDisable;
+      }
+      set {
+         cameraDisable = value;
+      }
+   }
+   public static bool FaceRotation {
+      get {
+         return faceRotation;
+      }
+      set {
+         faceRotation = value;
+      }
+   }
+   public static bool CubeRotation {
+      get {
+         return cubeRotation;
+      }
+      set {
+         cubeRotation = value;
+      }
+   }
+   public static bool GyroOn {
+      get {
+         return gyroOn;
+      }
+      set {
+         gyroOn = value;
+      }
+   }
+   public static bool Scrambling {
+      get {
+         return scrambling;
+      }
+      set {
+         scrambling = value;
+      }
+   }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+   public GameObject introScreen;
+   public GameObject chooseSizeScreen;
+   public GameObject chooseDifficulty;
+  
+
+   public void intoChooseSize(int index) {
+        switch (index)
+        {
+            case 1:
+                PlayerSettings.ScrambleRotationTime = 0.5f;
+                PlayerSettings.ScrambleTimes = 4;
+                break;
+            case 2:
+                PlayerSettings.ScrambleRotationTime = 0.35f;
+                PlayerSettings.ScrambleTimes = 7;
+                break;
+            case 3:
+                PlayerSettings.ScrambleRotationTime = 0.25f;
+                PlayerSettings.ScrambleTimes = 10;
+                break;
+            case 4:
+                PlayerSettings.ScrambleRotationTime = 0.15f;
+                PlayerSettings.ScrambleTimes = 13;
+                break;
+            case 5:
+                PlayerSettings.ScrambleRotationTime = 0.1f;
+                PlayerSettings.ScrambleTimes = 16;
+                break;
+            case 6:
+                PlayerSettings.ScrambleRotationTime = 0.08f;
+                PlayerSettings.ScrambleTimes = 19;
+                break;
+            case 7:
+                PlayerSettings.ScrambleRotationTime = 0.07f;
+                PlayerSettings.ScrambleTimes = 22;
+                break;
+            case 8:
+                PlayerSettings.ScrambleRotationTime = 0.06f;
+                PlayerSettings.ScrambleTimes = 25;
+                break;
+            case 9:
+                PlayerSettings.ScrambleRotationTime = 0.05f;
+                PlayerSettings.ScrambleTimes = 28;
+                break;
+            case 10:
+                PlayerSettings.ScrambleRotationTime = 0.04f;
+                PlayerSettings.ScrambleTimes = 30;
+                break;
+            default:
+                PlayerSettings.ScrambleRotationTime = 0.5f;
+                PlayerSettings.ScrambleTimes = 8;
+                break;
+
+        }
+      chooseDifficulty.SetActive(false);
+      chooseSizeScreen.SetActive(true);
+   }
+
+    public void intoChooseDifficulty()
+    {
+        introScreen.SetActive(false);
+        chooseDifficulty.SetActive(true);
+    }
+
+    public void LoadCube2(int index) {
+      PlayerSettings.CubeSize = 2;
+      SceneManager.LoadScene(index);
+   }
+   public void LoadCube3(int index) {
+      PlayerSettings.CubeSize = 3;
+      SceneManager.LoadScene(index);
+   }
+   public void LoadCube4(int index) {
+      PlayerSettings.CubeSize = 4;
+      SceneManager.LoadScene(index);
+   }
+
+   public void QuitGame() {
+      StopAllCoroutines();
+      Application.Quit();
+   }
+}
